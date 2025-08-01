f4c0c9cdb1aac5ac3c93e8522b659e6e10e72546
Alireza Aslani <a.aslani.work@gmail.com>
2025-08-01 14:41:29 +0330

    feat: add UploadWithFileSize function for file upload with size and type validation

--- upload_file/gin.go
+++ upload_file/gin.go
@@ -104,3 +104,85 @@ func Upload(c *gin.Context, params Params) (string, error) {
 
 	return filePath, nil
 }
+
+func UploadWithFileSize(c *gin.Context, params Params) (string, int64, error) {
+
+	fileHeader, err := c.FormFile(params.FieldName)
+	if err != nil {
+		if errors.Is(err, http.ErrMissingFile) {
+			if params.IsRequired {
+				return "", 0, ErrMissingFile
+			}
+			return "", 0, nil
+		}
+		return "", 0, err
+	}
+
+	if fileHeader.Size > params.MaxSize {
+		return "", 0, ErrFileSizeExceeds.Var(params.MaxSize)
+	}
+
+	mimeType := fileHeader.Header.Get("Content-Type")
+
+	isAccept := false
+
+	for _, a := range params.Accept {
+		if a == mimeType {
+			isAccept = true
+			break
+		}
+	}
+
+	if !isAccept {
+		return "", 0, ErrInvalidFileType.Var(mimeType)
+	}
+
+	var ext string
+	switch mimeType {
+	case "image/jpeg":
+		ext = "jpg"
+	case "image/png":
+		ext = "png"
+	case "application/pdf":
+		ext = "pdf"
+	case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+		ext = "docx"
+	case "application/msword":
+		ext = "doc"
+	case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+		ext = "xlsx"
+	case "application/vnd.ms-excel":
+		ext = "xls"
+	case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+		ext = "pptx"
+	case "application/vnd.ms-powerpoint":
+		ext = "ppt"
+	case "application/zip":
+		ext = "zip"
+	case "application/x-rar-compressed":
+		ext = "rar"
+	case "application/x-7z-compressed":
+		ext = "7z"
+	case "application/x-tar":
+		ext = "tar"
+	case "application/x-gzip":
+		ext = "gz"
+	case "application/x-bzip2":
+		ext = "bz2"
+	case "application/x-xz":
+		ext = "xz"
+	case "application/x-zip-compressed":
+		ext = "zip"
+	case "text/csv", "application/csv", "text/comma-separated-values":
+		ext = "csv"
+	default:
+		return "", 0, ErrInvalidFileType
+	}
+
+	filePath := fmt.Sprintf("%s/%s.%s", params.Path, uuid.NewString(), ext)
+	if err = c.SaveUploadedFile(fileHeader, filePath); err != nil {
+		return "", 0, err
+	}
+
+	return filePath, fileHeader.Size, nil
+}
