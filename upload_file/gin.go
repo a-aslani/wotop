72fe638a50c47671cc6758c2373c244c0b0ee2f6
Alireza Aslani <a.aslani.work@gmail.com>
2025-08-01 15:17:06 +0330

    feat: enhance file upload functionality with size, type validation, and temporary file support

--- upload_file/gin.go
+++ upload_file/gin.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
+	"os"
 )
 
 const (
@@ -16,11 +17,77 @@ const (
 )
 
 type Params struct {
-	FieldName  string
-	IsRequired bool
-	Path       string
-	MaxSize    int64
-	Accept     []string
+	FieldName   string
+	IsRequired  bool
+	Path        string
+	MaxSize     int64
+	Accept      []string
+	TempPattern *string
+	TempDir     *string
+}
+
+type Result struct {
+	FilePath string   `json:"file_path"`
+	FileSize int64    `json:"file_size"`
+	Temp     *os.File `json:"temp"`
+}
+
+func UploadFile(c *gin.Context, params Params) (*Result, error) {
+
+	fileHeader, err := c.FormFile(params.FieldName)
+	if err != nil {
+		if errors.Is(err, http.ErrMissingFile) {
+			if params.IsRequired {
+				return nil, ErrMissingFile
+			}
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	if fileHeader.Size > params.MaxSize {
+		return nil, ErrFileSizeExceeds.Var(params.MaxSize)
+	}
+
+	mimeType := fileHeader.Header.Get("Content-Type")
+
+	isAccept := false
+
+	for _, a := range params.Accept {
+		if a == mimeType {
+			isAccept = true
+			break
+		}
+	}
+
+	if !isAccept {
+		return nil, ErrInvalidFileType.Var(mimeType)
+	}
+
+	ext, err := getExt(mimeType)
+	if err != nil {
+		return nil, err
+	}
+
+	filePath := fmt.Sprintf("%s/%s.%s", params.Path, uuid.NewString(), ext)
+	if err = c.SaveUploadedFile(fileHeader, filePath); err != nil {
+		return nil, err
+	}
+
+	var tmp *os.File
+
+	if params.TempDir != nil && params.TempPattern != nil {
+		tmp, err = os.CreateTemp(*params.TempDir, *params.TempPattern)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &Result{
+		FilePath: filePath,
+		FileSize: fileHeader.Size,
+		Temp:     tmp,
+	}, nil
 }
 
 func Upload(c *gin.Context, params Params) (string, error) {
@@ -54,6 +121,21 @@ func Upload(c *gin.Context, params Params) (string, error) {
 	if !isAccept {
 		return "", ErrInvalidFileType.Var(mimeType)
 	}
+
+	ext, err := getExt(mimeType)
+	if err != nil {
+		return "", err
+	}
+
+	filePath := fmt.Sprintf("%s/%s.%s", params.Path, uuid.NewString(), ext)
+	if err = c.SaveUploadedFile(fileHeader, filePath); err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
+
+func getExt(mimeType string) (string, error) {
 
 	var ext string
 	switch mimeType {
@@ -97,92 +179,5 @@ func Upload(c *gin.Context, params Params) (string, error) {
 		return "", ErrInvalidFileType
 	}
 
-	filePath := fmt.Sprintf("%s/%s.%s", params.Path, uuid.NewString(), ext)
-	if err = c.SaveUploadedFile(fileHeader, filePath); err != nil {
-		return "", err
-	}
-
-	return filePath, nil
-}
-
-func UploadWithFileSize(c *gin.Context, params Params) (string, int64, error) {
-
-	fileHeader, err := c.FormFile(params.FieldName)
-	if err != nil {
-		if errors.Is(err, http.ErrMissingFile) {
-			if params.IsRequired {
-				return "", 0, ErrMissingFile
-			}
-			return "", 0, nil
-		}
-		return "", 0, err
-	}
-
-	if fileHeader.Size > params.MaxSize {
-		return "", 0, ErrFileSizeExceeds.Var(params.MaxSize)
-	}
-
-	mimeType := fileHeader.Header.Get("Content-Type")
-
-	isAccept := false
-
-	for _, a := range params.Accept {
-		if a == mimeType {
-			isAccept = true
-			break
-		}
-	}
-
-	if !isAccept {
-		return "", 0, ErrInvalidFileType.Var(mimeType)
-	}
-
-	var ext string
-	switch mimeType {
-	case "image/jpeg":
-		ext = "jpg"
-	case "image/png":
-		ext = "png"
-	case "application/pdf":
-		ext = "pdf"
-	case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-		ext = "docx"
-	case "application/msword":
-		ext = "doc"
-	case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-		ext = "xlsx"
-	case "application/vnd.ms-excel":
-		ext = "xls"
-	case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
-		ext = "pptx"
-	case "application/vnd.ms-powerpoint":
-		ext = "ppt"
-	case "application/zip":
-		ext = "zip"
-	case "application/x-rar-compressed":
-		ext = "rar"
-	case "application/x-7z-compressed":
-		ext = "7z"
-	case "application/x-tar":
-		ext = "tar"
-	case "application/x-gzip":
-		ext = "gz"
-	case "application/x-bzip2":
-		ext = "bz2"
-	case "application/x-xz":
-		ext = "xz"
-	case "application/x-zip-compressed":
-		ext = "zip"
-	case "text/csv", "application/csv", "text/comma-separated-values":
-		ext = "csv"
-	default:
-		return "", 0, ErrInvalidFileType
-	}
-
-	filePath := fmt.Sprintf("%s/%s.%s", params.Path, uuid.NewString(), ext)
-	if err = c.SaveUploadedFile(fileHeader, filePath); err != nil {
-		return "", 0, err
-	}
-
-	return filePath, fileHeader.Size, nil
+	return ext, nil
 }
